586d417c5c3a052fb6808a0743d134ff6110c52b
eLecCap1taL <Cap1taL07@outlook.com>
2026-08-01 13:48:37 +0800

    upd day2

---
+++ 26夏SDSC/Day2/模拟赛/SDSC2026D2/source/std/mutsumi/mutsumi.cpp
@@ -0,0 +1,250 @@
+//%^~
+// #pragma GCC optimize(3)
+// #pragma GCC optimize("Ofast")
+// #include <bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <array>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+#include <functional>
+#include <limits>
+#include <climits>
+#include <bitset>
+#include <cassert>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <random>
+#include <chrono>
+// #include "ext/pb_ds/assoc_container.hpp"
+// #include "ext/pb_ds/tree_policy.hpp"
+// #include "ext/pb_ds/priority_queue.hpp"
+// #include <ext/rope>
+// #define PBDS __gnu_pbds
+// #include <bits/extc++.h>
+#define MAXN 200005
+#define eps 1e-10
+#define foru(a, b, c) for (int a = (b); (a) <= (c); (a)++)
+#define ford(a, b, c) for (int a = (b); (a) >= (c); (a)--)
+#define uLL unsigned long long
+#define LL long long
+#define LXF int
+#define RIN Cap1taLDebug::read()
+#define RSIN Cap1taLDebug::rdstr()
+#define RCIN Cap1taLDebug::rdchar()
+#define HH printf("\n")
+#define All(x) (x).begin(), (x).end()
+#define fi first
+#define se second
+#define CA const auto&
+#if defined(__clang__)
+#define assume(expr) __builtin_assume(expr)
+#elif defined(__GNUC__)
+#define assume(expr) if (expr) {} else { __builtin_unreachable(); }
+#elif defined(_MSC_VER)
+#define assume(expr) __assume(expr)
+#endif
+#define likely(x) __builtin_expect(!!(x), 1) 
+#define unlikely(x) __builtin_expect(!!(x), 0)
+#define mkp(x,y) make_pair(x,y)
+#define ast(x) if(!(x))	{cerr<<endl<<"err at"<<__LINE__<<endl;exit(1);}
+#define sz(x) ((int)x.size())
+using namespace std;
+
+typedef __int128 i128;
+typedef unsigned __int128 u128;
+typedef long long i64;
+typedef unsigned long long u64;
+typedef int i32;
+typedef unsigned u32;
+typedef short i16;
+typedef unsigned short u16;
+
+class Cap1taLDebug{
+public:
+	#define DEBUGING
+
+	ostream& buf;
+	#ifndef DEBUGING
+	static char fbuf[1<<21],*p1,*p2;
+	#define getchar() (Cap1taLDebug::p1==p2&&(p1=(p2=fbuf)+fread(fbuf,1,1<<21,stdin),p1==p2)?EOF:*p2++)
+	#endif
+	Cap1taLDebug(ostream& out=cout):buf(out){}
+	~Cap1taLDebug(){
+		#ifdef DEBUGING
+		buf.flush();
+		#endif
+	}
+	static LXF read(){
+		LXF x=0,w=1;char ch=0;
+		while(!isdigit(ch)){
+		if(ch=='-')	w=-1;
+		ch=getchar();
+		}
+		while(isdigit(ch))	x=x*10+(ch^48),ch=getchar();
+		return x*w;
+	}
+	static string rdstr(){
+		string s;char c=getchar();
+		while(c==' ' || c=='\r' || c=='\n')	c=getchar();	
+		while(c!=' ' && c!='\r' && c!='\n' && c!=EOF)	s+=c,c=getchar();
+		return s;
+	}
+	static char rdchar(){
+		char c=getchar();
+		while(c==' ' || c=='\r' || c=='\n')	c=getchar();
+		return c;
+	}
+	static string i128ToString(i128 x){
+		if(x==0)	return "0";
+		string s="",w="";
+		if(x<0)	w="-",x*=-1;
+		while(x) s+=(char)('0'+(int)(x%10)),x/=10;
+		reverse(All(s));
+		return w+s;
+	}
+	static string u128ToString(u128 x){
+		if(x==0)	return "0";
+		string s="",w="";
+		while(x) s+=(char)('0'+(int)(x%10)),x/=10;
+		reverse(All(s));
+		return w+s;
+	}
+	Cap1taLDebug& operator<<(const string val){
+		#ifdef DEBUGING
+		buf<<val;
+		#endif
+		return *this;
+	}
+	template<typename T1,typename T2>
+	Cap1taLDebug& operator<<(const pair<T1,T2>& val){
+		#ifdef DEBUGING
+		(*this)<<"("<<val.first<<","<<val.second<<")";
+		#endif
+		return *this;
+	}
+	template<typename T,template<typename,typename...>class Container,typename...Args>
+	Cap1taLDebug& operator<<(const Container<T, Args...>& container){
+		#ifdef DEBUGING
+		buf<<"{";
+		bool fst=0;
+		for(const auto& val:container){
+			if(!fst)	fst=true;
+			else	buf<<",";
+			(*this)<<val;	
+		}
+		buf<<"}";
+		#endif
+		return *this;
+	}
+	Cap1taLDebug& operator<<(const i128& val){
+		#ifdef DEBUGING
+		buf<<i128ToString(val);
+		#endif
+		return *this;
+	}
+	Cap1taLDebug& operator<<(const u128& val){
+		#ifdef DEBUGING
+		buf<<u128ToString(val);
+		#endif
+		return *this;
+	}
+	template<typename T>
+	Cap1taLDebug& operator<<(const T& val){
+		#ifdef DEBUGING
+		buf<<val;
+		#endif
+		return *this;
+	}
+	Cap1taLDebug& operator<<(ostream& (*manip)(ostream&)){
+		#ifdef DEBUGING
+		buf<<manip;
+		#endif
+		return *this;
+	}
+};
+#ifndef DEBUGING
+char Cap1taLDebug::fbuf[1<<21],*Cap1taLDebug::p1=nullptr,*Cap1taLDebug::p2=nullptr;
+#endif
+Cap1taLDebug cein(cout);
+// Cap1taLDebug cein(cerr);
+ostream& operator<<(ostream& os,i128 val){
+	os<<Cap1taLDebug::i128ToString(val);
+	return os;
+}
+ostream& operator<<(ostream& os,u128 val){
+	os<<Cap1taLDebug::u128ToString(val);
+	return os;
+}
+
+
+template<typename T1,typename T2>
+inline bool chkmax(T1& x,const T2& y){return (T1)x<y?x=(T1)y,true:false;}
+template<typename T1,typename T2>
+inline bool chkmin(T1& x,const T2& y){return (T1)y<x?x=(T1)y,true:false;}
+
+
+/*
+
+*/
+int n;
+int a[805];
+int f[805][805];
+int dp[805];
+signed main()
+{
+	freopen("mutsumi.in","r",stdin);
+	freopen("mutsumi.out","w",stdout);
+	
+	n=RIN;
+	foru(i,1,n){
+		a[i]=RIN;
+	}
+	
+	foru(i,0,n+1){
+		foru(j,0,n+1){
+			f[i][j]=INT_MAX;
+		}
+	}
+	
+	for(int L=2;L<=n;L+=2){
+		for(int l=1;l+L-1<=n;l++){
+			int r=l+L-1;
+			
+			//在 l 这个位置放了比 l 大的数字，说明它在它该在的位置左侧，要完蛋了
+			if(a[l]>l)	continue;
+			if((l-a[l])%2!=0)	continue;
+			
+			int res=(l-a[l])/2;
+			
+			for(int k=l+1;k<r;k+=2){
+				if(f[l][k]==INT_MAX || f[k+1][r]==INT_MAX)	continue;
+				chkmin(f[l][r],max({res,f[l][k],f[k+1][r]-(k-l+1)/2}));
+			}
+			
+			if(L==2 || f[l+1][r-1]<=res){
+				chkmin(f[l][r],res);
+			}
+		}
+	}
+	
+	foru(i,1,n){
+		chkmax(dp[i],dp[i-1]);
+		for(int j=i-2;j>=0;j-=2){
+			if(f[j+1][i]<=dp[j]){
+				chkmax(dp[i],dp[j]+(i-j)/2);
+			}
+		}
+	}
+	
+	cout<<dp[n]<<endl;
+	return 0;
+}
